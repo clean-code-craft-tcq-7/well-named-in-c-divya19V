f04f7d26a62002460411c61c139d701b5ad871c9
DivyaKaushik <38903055+divya19V@users.noreply.github.com>
2022-11-22 23:28:20 +0530

    Added comments to improve code readability

--- 25_pair_code_functions_.c
+++ 25_pair_code_functions_.c
@@ -1,9 +1,13 @@
+//File contains functions to return major color, minor color and number 
 #include <stdio.h>
 #include <assert.h>
 #include "25_pair_code_functions.h"
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
+//Function tests pair number to a expected major and minor color
+Input: pair number, expected major color and expected minor color
+Return type: void
 void testNumberToPair(int pairNumber,
     enum MajorColor expectedMajor,
     enum MinorColor expectedMinor)
@@ -16,6 +20,9 @@ void testNumberToPair(int pairNumber,
     assert(colorPair.minorColor == expectedMinor);
 }
 
+//Function tests major and minor color to expected pair number
+Input: major color, minor color and expected pair number
+Return type: void
 void testPairToNumber(
     enum MajorColor major,
     enum MinorColor minor,
@@ -30,7 +37,7 @@ void testPairToNumber(
 }
 
 int main() {
-    printtable();
+    printTable();
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
     testPairToNumber(BLACK, ORANGE, 12);
